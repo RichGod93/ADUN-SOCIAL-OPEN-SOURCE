5852bbf715d9f771a31d6cc91aa50ec88ac28a30
RichGod <usenrichgod93@gmail.com>
2023-02-22 01:03:37 +0100

    set up tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
