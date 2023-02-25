73b12dfa9c0e64322d896d683fc3034da8b06e7f
RichGod <usenrichgod93@gmail.com>
2023-02-25 09:13:11 +0100

    add: better style

--- src/components/landingPage/Logo.tsx
+++ src/components/landingPage/Logo.tsx
@@ -1,10 +1,10 @@
 const Logo = () => {
     return (
-        <div className="flex justify-between items-center dark:primary-bg-color py-1 px-2">
+        <div className="flex justify-between items-center py-1 px-2">
             <div className="cursor-pointer flex items-center">
-                <p className="font-bold primary-text-color dark:secondary-text-color pr-1">ADUN</p>
-                <div className="h-6 w-16 primary-bg-color dark:secondary-bg-color rounded-lg flex items-center justify-center">
-                    <p className="font-medium secondary-text-color dark:primary-text-color">SOCIAL</p>
+                <p className="font-bold primary-text-color pr-1">ADUN</p>
+                <div className="h-6 w-16 primary-bg-color rounded-lg flex items-center justify-center">
+                    <p className="font-medium secondary-text-color">SOCIAL</p>
                 </div>
             </div>
         </div>);
