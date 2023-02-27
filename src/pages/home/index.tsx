175553d5c28941e226504c6b24ccca10d0da2a10
RichGod <usenrichgod93@gmail.com>
2023-02-27 01:40:33 +0100

    add: layout logic

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,22 @@
-import Head from "next/head";
+import type { ReactElement } from 'react';
+import { Layout } from '@/components';
+import type { NextPageWithLayout } from '../../pages/_app';
+import { appwrite } from 'config/appwriteConfig';
+import { useRouter } from 'next/router';
 
-const HomePage = () => {
-    return (<div></div>);
+
+const HomePage: NextPageWithLayout = () => {
+    const router = useRouter();
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+HomePage.getLayout = function getLayout(page: ReactElement) {
+    return (<Layout>{page}</Layout>);
 };
 
 export default HomePage;
