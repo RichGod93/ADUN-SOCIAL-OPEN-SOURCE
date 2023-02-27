b09aa3e52b9e00aafb9a7fcc42b0df6e9e6abbb6
RichGod <usenrichgod93@gmail.com>
2023-02-27 01:39:06 +0100

    add: page layout logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,25 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    getLayout(
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    )
+
   );
 }
