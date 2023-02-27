91d9f22dc0ccdd2dc9e29251e7860348d1f3e7eb
RichGod <usenrichgod93@gmail.com>
2023-02-27 01:38:18 +0100

    add: layout logic

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,7 +1,14 @@
-const Layout = () => {
+import NavBar from "./NavBar";
+
+type DefaultLayoutProps = {
+    children: React.ReactNode,
+};
+
+const Layout = ({ children }: DefaultLayoutProps) => {
     return (
-        <div>
-            <div>{ }</div>
+        <div className="h-screen px-5 md:lg:px-10 flex flex-col bg-gradient-to-b light-gradient dark:dark-gradient">
+            <NavBar />
+            <div>{children}</div>
         </div>
     );
 };
