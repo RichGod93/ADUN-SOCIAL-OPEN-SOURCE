24291479835bfe5575c396d543dea656495075c7
RichGod <usenrichgod93@gmail.com>
2023-02-27 01:37:38 +0100

    add: NavBar

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,14 +3,18 @@ import PageHead from "./PageHead";
 
 // Landing page components
 import Hero from "./landingPage/Hero";
+import Logo from "./landingPage/Logo";
 
 // Layout
 import Layout from "./layouts/Layout";
+import NavBar from "./layouts/NavBar";
 
 export {
     PageHead,
 
     Hero,
+    Logo,
 
-    Layout
+    Layout,
+    NavBar
 };
