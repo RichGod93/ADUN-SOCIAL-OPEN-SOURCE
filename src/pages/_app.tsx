9b246aab15e4a754a6ee157a5f1dce27ee55d8f4
RichGod <usenrichgod93@gmail.com>
2023-03-02 02:26:05 +0100

    del: RecoilRoot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
 
 import { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
@@ -17,9 +16,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     getLayout(
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <Component {...pageProps} />
     )
 
   );
