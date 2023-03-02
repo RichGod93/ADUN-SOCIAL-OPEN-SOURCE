08f3ac9448993675a7d625f01c99e45d6d8ab604
RichGod <usenrichgod93@gmail.com>
2023-03-02 19:36:57 +0100

    add: toast container

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,5 +1,8 @@
 import NavBar from "./NavBar";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 type DefaultLayoutProps = {
     children: React.ReactNode,
 };
@@ -9,6 +12,19 @@ const Layout = ({ children }: DefaultLayoutProps) => {
         <div className="h-screen px-5 md:lg:px-10 flex flex-col bg-gradient-to-b light-gradient dark:dark-gradient">
             <NavBar />
             <div>{children}</div>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
         </div>
     );
 };
