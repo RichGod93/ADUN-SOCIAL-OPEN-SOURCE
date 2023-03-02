7a19fbc1d6ea8cb1ef2bd3bc0c30acd85e7a8469
RichGod <usenrichgod93@gmail.com>
2023-03-02 02:25:31 +0100

    del: atom, recoil, types

--- config/appwriteConfig.tsx
+++ config/appwriteConfig.tsx
@@ -1,6 +1,4 @@
 import { Client as Appwrite, Account, Databases } from "appwrite";
-import { atom } from 'recoil';
-import { User } from "./types";
 
 export const Server = {
     endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '',
@@ -13,12 +11,3 @@ const account = new Account(client);
 // const database = new Databases(client, Server.databaseID);
 
 export const appwrite = { account };
-
-export const userState = atom<User>({
-    key: "user",
-    default: {
-        $id: "",
-        name: "",
-        email: ""
-    }
-});
