e30376cabf465e6b30b792df983e962cc9c9fbcb
RichGod <usenrichgod93@gmail.com>
2023-03-07 22:46:54 +0100

    styling: responsive

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
         <nav className="w-full flex items-center justify-between py-2">
             <div className=""><Logo /></div>
 
-            <ul className="hidden lg:flex lg:flex-1 items-center justify-center lg:space-x-10">
+            <ul className="hidden md:flex md:flex-1 lg:flex lg:flex-1 items-center justify-center lg:space-x-10">
                 <li className="py-3 px-5 cursor-pointer hover:border-b-2 hover:primary-border-color">
                     <HomeIcon className="icon-small primary-text-color" />
                 </li>
@@ -41,10 +41,10 @@ const NavBar = () => {
             </ul>
 
             <div className="flex items-center space-x-1">
-                <UserCircleIcon className="hidden lg:block icon-medium primary-text-color" />
+                <UserCircleIcon className="hidden md:block lg:block icon-medium primary-text-color" />
                 <button
                     onClick={logout}
-                    className="hidden lg:block w-full px-4 py-2 text-sm secondary-text-color primary-bg-color"
+                    className="hidden md:block lg:block w-full px-4 py-2 text-sm secondary-text-color primary-bg-color"
                 >
                     Logout 👋
                 </button>
