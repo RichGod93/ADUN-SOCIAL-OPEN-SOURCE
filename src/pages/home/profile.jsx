f7b2c60bc98b2fbae779aa9ed86f728785804338
Abdulmojeed <mojeedayoola58@gmail.com>
2023-09-26 02:40:56 +0100

    chore: improve the authentication

--- src/pages/home/profile.jsx
+++ src/pages/home/profile.jsx
@@ -5,120 +5,123 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { auth, db } from "../../../config/firebaseConfig";
 import { ToastContainer } from "react-toastify";
-
-
+import PrivateRoute from "@/components/PrivateRoute";
 
 const ProfilePage = () => {
-    const { currentUser } = useAuth();
+  const { currentUser } = useAuth();
 
-    const [userType, setUserType] = useState("");
+  const [userType, setUserType] = useState("");
 
-    const filepickerRef = useRef(null);
-    const [photo, setPhoto] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [photoURL, setPhotoURL] = useState("/avatar.png");
+  const filepickerRef = useRef(null);
+  const [photo, setPhoto] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [photoURL, setPhotoURL] = useState("/avatar.png");
 
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setPhoto(e.target.files[0]);
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setPhoto(e.target.files[0]);
+    }
+  };
+  const handleClick = async () => {
+    const url = await upload(photo, currentUser, setLoading);
+    setPhotoURL(url);
+    removeImage();
+    if (url) {
+      toast.success("Profile picture uploaded");
+    } else {
+      toast.error("Upload failed, please try again");
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser?.photoURL) {
+      setPhotoURL(currentUser.photoURL);
+    }
+  }, [currentUser]);
+
+  const removeImage = () => {
+    setPhoto(null);
+  };
+
+  useEffect(() => {
+    getDoc(doc(collection(db, "user_type"), auth.currentUser?.uid)).then(
+      (snapshot) => {
+        if (snapshot) {
+          setUserType(snapshot.data());
         }
-    };
-    const handleClick = async () => {
-        const url = await upload(photo, currentUser, setLoading);
-        setPhotoURL(url);
-        removeImage();
-        if (url) {
-            toast.success("Profile picture uploaded");
-        } else {
-            toast.error("Upload failed, please try again");
-        }
-    };
-
-    useEffect(() => {
-        if (currentUser?.photoURL) {
-            setPhotoURL(currentUser.photoURL);
-        }
-    }, [currentUser]);
-
-    const removeImage = () => {
-        setPhoto(null);
-    };
-
-    useEffect(() => {
-        getDoc(doc(collection(db, "user_type"), auth.currentUser?.uid)).then(
-            (snapshot) => {
-                if (snapshot) {
-                    setUserType(snapshot.data());
-                }
-            }
-        );
-    }, []);
-
-    return (
-        <>
-            <PageHead title="ADUN Social" page_name="Profile" />
-            <HomeLayout>
-                <div className="flex flex-col justify-between px-2 space-y-3">
-                    <div className="flex flex-col items-center space-y-2">
-                        <div className="flex items-center">
-                            <Image
-                                src={photoURL}
-                                height={200}
-                                width={200}
-                                layout="fixed"
-                                objectFit="cover"
-                                alt="Profile Image"
-                                className="rounded-full border-2 border-gray-400"
-                            />
-                            <div className="flex flex-col">
-                                <div
-                                    className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-gray-200 dark:hover:bg-slate-800 cursor-pointer self-end"
-                                    onClick={() => filepickerRef.current.click()}
-                                >
-                                    {/* <AddAPhotoIcon /> */}
-                                    <input
-                                        ref={filepickerRef}
-                                        type="file"
-                                        onChange={handleChange}
-                                        hidden
-                                    />
-                                </div>
-                                {photo && (
-                                    <div
-                                        onClick={removeImage}
-                                        className="cursor-pointer self-end"
-                                    >
-                                        <p className="text-xs text-red-500 text-center">
-                                            Remove <br />
-                                            Image
-                                        </p>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-
-                        <button
-                            disabled={loading || !photo}
-                            className="px-10 py-2 primary-bg-color dark:secondary-bg-color secondary-text-color dark:primary-text-color rounded-sm font-medium"
-                            onClick={handleClick}
-                        >
-                            Upload Profile Picture
-                        </button>
-                    </div>
-
-                    {currentUser?.admin ? (
-                        <div className="flex items-center justify-center">
-                            <p className="text-xs uppercase font-bold">{userType.type}</p>
-                        </div>
-                    ) : (
-                        <></>
-                    )}
-                    {/* {userType.type === "student" ? <StudentInfo /> : <StaffInfo />} */}
-                </div>
-                <ToastContainer />
-            </HomeLayout>
-        </>
+      }
     );
+  }, []);
+  console.log(photoURL)
+
+  return (
+    <PrivateRoute>
+      <PageHead title="ADUN Social" page_name="Profile" />
+      <HomeLayout>
+        <div className="flex flex-col justify-between px-2 space-y-3">
+          <div className="flex flex-col items-center space-y-2">
+            <div className="flex items-center">
+              <Image
+                src={
+                  photoURL ? photoURL :
+                  "https://www.habeebat.com/wp-content/uploads/2016/08/dummy-prod-1.jpg"
+                }
+                height={200}
+                width={200}
+                layout="fixed"
+                objectFit="cover"
+                alt="Profile Image"
+                className="rounded-full border-2 border-gray-400"
+              />
+              <div className="flex flex-col">
+                <div
+                  className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-gray-200 dark:hover:bg-slate-800 cursor-pointer self-end"
+                  onClick={() => filepickerRef.current.click()}
+                >
+                  {/* <AddAPhotoIcon /> */}
+                  <input
+                    ref={filepickerRef}
+                    type="file"
+                    onChange={handleChange}
+                    hidden
+                  />
+                </div>
+                {photo && (
+                  <div
+                    onClick={removeImage}
+                    className="cursor-pointer self-end"
+                  >
+                    <p className="text-xs text-red-500 text-center">
+                      Remove <br />
+                      Image
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <button
+              disabled={loading || !photo}
+              className="px-10 py-2 primary-bg-color dark:secondary-bg-color secondary-text-color dark:primary-text-color rounded-sm font-medium"
+              onClick={handleClick}
+            >
+              Upload Profile Picture
+            </button>
+          </div>
+
+          {currentUser?.admin ? (
+            <div className="flex items-center justify-center">
+              <p className="text-xs uppercase font-bold">{userType.type}</p>
+            </div>
+          ) : (
+            <></>
+          )}
+          {/* {userType.type === "student" ? <StudentInfo /> : <StaffInfo />} */}
+        </div>
+        <ToastContainer />
+      </HomeLayout>
+    </PrivateRoute>
+  );
 };
 
 export default ProfilePage;
